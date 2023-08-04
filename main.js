93ccaa002794326338a0df5771f996e1d1feeaad
Tata123Expert <117285408+Tata123Expert@users.noreply.github.com>
2023-08-04 16:56:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(500, 420);
+    canvas.position(510, 280);
+    video = createCapture(VIDEO);
+    video.hide();
+    tintColor = "";
+}
+
+function draw()
+{
+    image(video, 0, 0, 500, 420);
+    tint(tintColor);
+}
+
+function takeSnapshot()
+{
+    save('SuaFoto.png');
+}
+
+function filterTint()
+{
+    tintColor = document.getElementById("colorName").value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
